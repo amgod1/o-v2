a61cd27727badd734cf3cf433ef79bf9b829024a
amgod1 <kamillschepuk@gmail.com>
2023-11-08 23:22:59 +0300

    small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react"
 import { Header } from "./components/Header"
 import { Settings } from "./components/Settings"
 import { Ticket } from "./components/Ticket"
 import { ModalContextProvider } from "./context/ModalContext"
 import { TicketContextProvider } from "./context/TicketContext"
+import { x } from "./constants/x"
 
 export const App = () => {
+  useEffect(() => {
+    localStorage.setItem("x", x)
+    console.log("done")
+  }, [])
+
   return (
     <ModalContextProvider>
       <TicketContextProvider>
