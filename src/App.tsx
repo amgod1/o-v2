e352b8d891d51f5da7f18aa2c14648cb15568917
amgod1 <kamillschepuk@gmail.com>
2023-11-08 23:34:04 +0300

    fix: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react"
 import { Header } from "./components/Header"
 import { Settings } from "./components/Settings"
 import { Ticket } from "./components/Ticket"
@@ -6,19 +5,17 @@ import { ModalContextProvider } from "./context/ModalContext"
 import { TicketContextProvider } from "./context/TicketContext"
 
 export const App = () => {
-  if (
-    localStorage.getItem("x") !==
-    "8ec13e0a9aeb4a648c0d3e6f3c82cde7a9f51e8f18cd4cf988b70e38a3a825d8b0c4d6c5c3c4e0aa39a5e8db1605fa0c4f40f9402d3d4b269bbd03d1c0d80e3691c29502da2b0b8202a9t1"
-  ) {
-    return <span>x</span>
-  }
-
   return (
     <ModalContextProvider>
       <TicketContextProvider>
-        <Header />
-        <Ticket />
-        <Settings />
+        {localStorage.getItem("x") ===
+          "8ec13e0a9aeb4a648c0d3e6f3c82cde7a9f51e8f18cd4cf988b70e38a3a825d8b0c4d6c5c3c4e0aa39a5e8db1605fa0c4f40f9402d3d4b269bbd03d1c0d80e3691c29502da2b0b8202a9t1" && (
+          <>
+            <Header />
+            <Ticket />
+            <Settings />
+          </>
+        )}
       </TicketContextProvider>
     </ModalContextProvider>
   )
